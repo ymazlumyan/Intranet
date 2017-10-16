5a694739c47f56e0eccd7beae0cfd90bfd752a96
Yervand Mazlumyan <ymazlumyan@hotmail.com>
2017-10-16 09:30:30 -0700

    Adding and modifying inital files

--- Nv.Tax.Intranet/App_Start/BundleConfig.cs
+++ Nv.Tax.Intranet/App_Start/BundleConfig.cs
@@ -21,6 +21,7 @@ namespace Nv.Tax.Intranet
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/main.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
---
+++ Nv.Tax.Intranet/Helpers/CustomHelpers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Nv.Tax.Intranet.Helpers
+{
+    public static class CustomHelpers
+    {
+        public static IHtmlString MenuItem(this HtmlHelper helper, string title, object htmlAttributes)
+        {
+            var span = new TagBuilder("span");
+
+            span.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+
+            return MvcHtmlString.Create(@"<a href='#'>" + span + title + "</a>");
+        }
+
+        public static IHtmlString MenuItem(this HtmlHelper html, string title, string action, object htmlAttributes)
+        {
+            
+            var span = new TagBuilder("span");
+
+            span.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+
+            return MvcHtmlString.Create(string.Format("<a href='/{0}/{1}'>" + span + title + "</a>", html.ViewContext.RouteData.GetRequiredString("controller"), action));
+        }
+
+        public static IHtmlString MenuItem(this HtmlHelper html, string title, string action, string controller, object htmlAttributes)
+        {
+            var span = new TagBuilder("span");
+
+            span.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+
+            return MvcHtmlString.Create(string.Format("<a href='/{0}/{1}'>" + span + title + "</a>", controller, action));
+        }
+    }
+}
